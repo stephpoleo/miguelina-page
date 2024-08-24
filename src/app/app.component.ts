bcbeb78adb434cf2fa435b9f6826a424f69a698e
stephpoleo <stephaniepoleo@hotmail.com>
2024-08-24 09:54:44 -0600

    Deploy using Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PresentationComponent } from './presentation/presentation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PresentationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
